4f5ce9bbf20ee2bd77ae1b9c910de1059297bb4d
HP <HP@LAPTOP-B7JDQTQE>
2022-09-26 16:09:18 +0700

    Add project files.

---
+++ P2_1_1214060/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1214060
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+    {
+            Console.WriteLine("kalkulator prisya");
+            Console.Write("input nilai satu: ");
+            int angka1 = int.Parse(Console.ReadLine());
+            Console.Write("input nilai dua: ");
+            int angka2 = int.Parse(Console.ReadLine());
+
+            double penjumlahan = angka1 + angka2;
+            Console.WriteLine("{0} + {1} = {2}", angka1, angka2, penjumlahan);
+
+            double pengurangan = angka1 - angka2;
+            Console.WriteLine("{0} - {1} = {2}", angka1, angka2, pengurangan);
+
+            double perkalian = angka1 * angka2;
+            Console.WriteLine("{0} * {1} = {2}", angka1, angka2, perkalian);
+
+            double pembagian = angka1 / angka2;
+            Console.WriteLine("{0} / {1} = {2}", angka1, angka2, pembagian);
+
+        }
+    }
+}
